20dfae1d94bdc40d29bac6ede71b3972c0b618d4
Amee Leonor <munkoev.e@yandex.ru>
2022-08-26 12:12:28 +0300

    fix: eslint config, readme

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # e-store
 practicing with react
+A simple e-store using free public API https://fakestoreapi.com/docs
+
+- stack:
+  - react
+  - redux toolkit
+  - jest
+  - typescript
+  - scss
+  - eslint airbnb
--- src/.eslintrc.js
+++ src/.eslintrc.js
@@ -1,4 +1,4 @@
-{
+module.exports = {
     "env": {
         "browser": true,
         "es2021": true
@@ -12,7 +12,8 @@
     "parserOptions": {
         "ecmaVersion": "latest",
         "sourceType": "module",
-        "project": "./tsconfig.json"
+        "project": "./tsconfig.json",
+        "tsconfigRootDir": __dirname
     },
     "plugins": [
         "react"
